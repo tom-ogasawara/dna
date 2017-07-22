779c4cb6d62852d3f2767037cd610f0f32619638
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-07-22 15:30:23 -0700

    Set up app skeleton

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Header</h2>
+        </div>
+        Body
+      </div>
+    );
+  }
+}
+
+export default App;
